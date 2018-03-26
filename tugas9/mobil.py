da62ba39cd2d2e991f5ae3603af4d6397d7d1695
ayamberkakienam <ayamberkakienam@users.noreply.github.com>
2018-03-26 22:40:38 +0700

    Finally a spin that works

--- tugas9/mobil.py
+++ tugas9/mobil.py
@@ -2,15 +2,14 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-from OpenGL.GL import *
-from OpenGL.GLUT import *
-from OpenGL.GLU import *
+from time import sleep
 
 window = 0                                             # glut window number
 width, height = 800, 600                               # window size
 
 def draw_rect(x, y, width, height):
     glBegin(GL_QUADS)                                  # start drawing a rectangle
+    glColor3f(0.0, 0.0, 1.0)                           # set color to blue
     glVertex2f(x, y)                                   # bottom left point
     glVertex2f(x + width, y)                           # bottom right point
     glVertex2f(x + width, y + height)                  # top right point
@@ -21,20 +20,47 @@ def refresh2d(width, height):
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
+
     glOrtho(0.0, width, 0.0, height, 0.0, 1.0)
     glMatrixMode (GL_MODELVIEW)
     glLoadIdentity()
 
+vertices_wheel = (
+    (300, 300),
+    (100, 300),
+    (100, 100),
+    (300, 100),
+    (400, 400)
+    )
+
+def Wheel():
+    glPushMatrix()
+    glRotate(x, 0, 0, 1)
+    glBegin(GL_POLYGON)
+    glColor3f(1, 1, 1)
+    for vertex in vertices_wheel:
+        glVertex2fv(vertex)
+    glEnd()  
+    glPopMatrix()
+
+x = 10
 def draw():                                            # ondraw is called all the time
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen
     glLoadIdentity()                                   # reset position
     refresh2d(width, height)                           # set mode to 2d
        
-    glColor3f(0.0, 0.0, 1.0)                           # set color to blue
-    draw_rect(10, 10, 200, 100)                        # rect at (10, 10) with width 200, height 100
-       
+    Wheel()
+    draw_rect(100, 100, 200, 100)                        # rect at (10, 10) with width 200, height 100
+        
+
     glutSwapBuffers()                                  # important for double buffering
    
+def idle():
+    global x
+    x += 10
+    glutPostRedisplay()
+    
+
 
 # initialization
 glutInit()                                             # initialize glut
@@ -43,5 +69,5 @@ glutInitWindowSize(width, height)                      # set window size
 glutInitWindowPosition(0, 0)                           # set window position
 window = glutCreateWindow("noobtuts.com")              # create window with title
 glutDisplayFunc(draw)                                  # set draw function callback
-glutIdleFunc(draw)                                     # draw all the time
+glutIdleFunc(idle)                                     # draw all the time
 glutMainLoop()                                         # start everything
