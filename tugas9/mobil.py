d693ed37642dcea2c45eda7f040819a4b19214d4
ayamberkakienam <ayamberkakienam@users.noreply.github.com>
2018-03-26 23:54:20 +0700

    Create grafikautils so main program wont be too crowded Cre

--- tugas9/mobil.py
+++ tugas9/mobil.py
@@ -1,3 +1,5 @@
+import grafikautils as utils
+
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
@@ -6,24 +8,6 @@ from time import sleep
 
 window = 0                                             # glut window number
 width, height = 800, 600                               # window size
-
-def refresh2d(width, height):
-    glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-
-    glOrtho(0.0, width, 0.0, height, 0.0, 1.0)
-    glMatrixMode (GL_MODELVIEW)
-    glLoadIdentity()
-
-def draw_rect(x, y, width, height):
-    glBegin(GL_QUADS)                                  # start drawing a rectangle
-    glColor3f(0.0, 0.0, 1.0)                           # set color to blue
-    glVertex2f(x, y)                                   # bottom left point
-    glVertex2f(x + width, y)                           # bottom right point
-    glVertex2f(x + width, y + height)                  # top right point
-    glVertex2f(x, y + height)                          # top left point
-    glEnd()
 
 vertices_wheel = (
     (300, 300),
@@ -57,7 +41,7 @@ vertices_body = ( # +230, +350
 def obj_body():
     glShadeModel(GL_SMOOTH)
     glBegin(GL_POLYGON)
-    glColor3fv((1, 1, 1))
+    glColor3fv((0.5, 0.8, 0.2))
     for vertex in vertices_body:
         glVertex2fv(vertex)
     glEnd()
@@ -66,10 +50,11 @@ x = 10
 def draw():                                            # ondraw is called all the time
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen
     glLoadIdentity()                                   # reset position
-    refresh2d(width, height)                           # set mode to 2d
+    utils.refresh2d(width, height)                           # set mode to 2d
        
     # obj_wheel()
     obj_body()
+    utils.draw_circle(100, 100, 50, 100, 0)
     # draw_rect(100, 100, 200, 100)                        # rect at (10, 10) with width 200, height 100
         
 
